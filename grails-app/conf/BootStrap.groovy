0dc124b731085f46b8e87f7d5f66c8a758e8b130
student <student@WebArch.localdomain>
2013-05-05 16:53:26 +0100

    Set up scaffold, editboot strap files and add an entry for Instructor

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
+import uk.ac.shu.webarch.eregister.*;
+
 class BootStrap {
 
     def init = { servletContext ->
+	println("BootStrap::init");
+	def chris_instructor = Instructor.findByStaffId('000001a');
+
+	if(chris_instructor == null){
+	println("Instructor not found, create...");
+	chris_instructor = new Instructor()
+	chris_instructor.staffId = '000001a';
+	chris_instructor.name = 'Chris Shelley';
+	chris_instructor.save();
+}
+	else{
+	println("Result of find by staffId for chris: ${chris_instructor}");
     }
+}
     def destroy = {
     }
 }
